7785efb75aa0119201f05526ad926aa96c16c0a5
Miroslav Taleiko <mtaleiko@gmail.com>
2022-11-23 10:06:40 +0300

    Initial Commit

---
+++ SImpleHN/SImpleHNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SImpleHNApp.swift
+//  SImpleHN
+//
+//  Created by Miroslav Taleiko on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SImpleHNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
