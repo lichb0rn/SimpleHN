be36f0457ff86df26431a4d189a289a9a9deb6d5
Miroslav Taleiko <mtaleiko@gmail.com>
2023-01-02 06:17:58 +0300

    Small animation changes.

--- SImpleHN/Domain/Stories/Scene/Stories List/StartupView.swift
+++ SImpleHN/Domain/Stories/Scene/Stories List/StartupView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct StartupView: View {
     @State private var isAnimating = false
     @State private var scale = 1.0
-
+    
     
     var body: some View {
         ZStack {
@@ -18,7 +18,7 @@ struct StartupView: View {
                 .ignoresSafeArea(.all)
             
             HNLogoView()
-                .scaleEffect(x: 1.0 / scale, y: scale, anchor: .center)
+                .scaleEffect(x: 1.0 / scale, y: scale, anchor: .bottom)
                 .offset(y: yOffset)
                 .onAppear {
                     animate()
@@ -27,7 +27,7 @@ struct StartupView: View {
     }
     
     private var yOffset: CGFloat {
-        isAnimating ? -100 : 0
+        isAnimating ? -150 : 0
     }
     
     private var duration: CGFloat = 0.5
@@ -37,31 +37,29 @@ struct StartupView: View {
         }
         
         withAnimation(.easeOut(duration: duration).repeatForever()) {
-            scale = 0.85
+            scale = 0.9
         }
     }
 }
 
 struct HNLogoView: View {
     var body: some View {
-
-            
-            Text("Y")
-                .font(.custom("Helvetica", size: 128, relativeTo: .title))
-                .padding()
-                .foregroundColor(.white)
-                .background {
-                    Rectangle()
-                        .aspectRatio(contentMode: .fill)
-                        .foregroundColor(Color("MainColor"))
-                        .border(Color.white, width: 10)
-                        .overlay {
-                            Rectangle()
-                                .foregroundColor(.clear)
-                                .border(Color("MainColor"), width: 4)
-                        }
-                }
-        }
+        Text("Y")
+            .font(.custom("Helvetica", size: 128, relativeTo: .title))
+            .padding()
+            .foregroundColor(.white)
+            .background {
+                Rectangle()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color("MainColor"))
+                    .border(Color.white, width: 10)
+                    .overlay {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .border(Color("MainColor"), width: 4)
+                    }
+            }
+    }
 }
 
 struct StartupView_Previews: PreviewProvider {
